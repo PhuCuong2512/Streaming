f2138981f0abee403a3afc6b884dab757c87b965
PhuCuong2512 <131945083+PhuCuong2512@users.noreply.github.com>
2024-06-19 19:35:42 +0700

    Dark Mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+export default function Page() {
   return (
-    <p>
-      <div></div>
-    </p>
+    <div>
+      <div className="flex flex-col gap-y-4">
+        <h1>Dashboard</h1>
+      </div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
   );
 }
