7895fb1c497a98e7eaff7ae9b4e9e0ef8b346560
PhuCuong2512 <131945083+PhuCuong2512@users.noreply.github.com>
2024-07-22 12:47:15 +0700

    Navigation User

---
+++ app/(browse)/_components/navbar/index.tsx
@@ -0,0 +1,14 @@
+import { Logo } from "./logo";
+import { Search } from "./search";
+import { Actions } from "./actions";
+
+export const Navbar = () => {
+  return (
+    <nav className="fixed top-0 w-full h-20 z-[49] bg-[#252731] px-2 lg:px-4 flex justify-between items-center shadow-sm">
+      <Logo />
+      <Search />
+      <Actions />
+    </nav>
+  );
+};
+// Compare this snippet from app/%28browse%29/_components/navbar/index.tsx:
