230048964f2be970ec81bf8db5a5131672896167
PhuCuong2512 <PhuCuong2512>
2024-09-16 16:08:36 +0700

    Setting Chat

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -59,8 +59,12 @@ export async function POST(req: Request) {
         externalUserId: payload.data.id,
         username: payload.data.username,
         imageUrl: payload.data.image_url,
+        stream: {
+          create: {
+            name: `${payload.data.username}'s stream`,
+        }
       },
-    });
+    }});
   }
   if (eventType === "user.updated") {
     const currentUser = await db.user.findUnique({
